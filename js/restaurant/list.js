8d9534db68dc8924aabdbdda123663a22918f848
Tom Yeh <doubleshow@gmail.com>
2015-03-08 05:22:12 -0600

    build

---
+++ js/restaurant/list.js
@@ -0,0 +1,11 @@
+var RestaurantList = React.createClass({displayName: 'RestaurantList',
+
+    render: function() {
+
+        return (
+            <div>
+                TODO
+            </div>
+        )
+    }
+})
---
+++ js/restaurant/view.js
@@ -0,0 +1,11 @@
+var RestaurantView = React.createClass({
+
+    render: function() {
+
+        return (
+            <div>
+                TODO
+            </div>
+        )
+    }
+})
