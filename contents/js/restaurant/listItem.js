6fe5afb3075c26b0569bfc701080eeee6dcdff42
Irakli Zhuzhunashvili <irakli.zhuzhunashvili1@gmail.com>
2015-03-09 16:13:06 -0600

    Added listing restaurants

--- contents/js/restaurant/listItem.js
+++ contents/js/restaurant/listItem.js
@@ -1,10 +1,36 @@
 var RestaurantListItem = React.createClass({
 
+    handleClick: function(event) {
+        console.log('an item is clicked', this.props.restaurant)
+        this.props.onListItemClicked(this.props.restaurant.business_id)
+    },
+
     render: function() {
+        
+        var restaurant = this.props.restaurant
 
         return (
-            <div>
-                TODO
+            <div className="doctor">
+                <table className="u-full-width">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>State</th>
+                            <th>City</th>
+                            <th>Rating</th>
+                            <th>Open</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><a href="#" onClick={this.handleClick}>{restaurant.name}</a></td>
+                            <td> {restaurant.state} </td>
+                            <td> {restaurant.city} </td>
+                            <td> {restaurant.stars + " Stars"} </td>
+                            <td> {restaurant.open} </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         )
     }
