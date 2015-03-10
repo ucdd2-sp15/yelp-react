7567a43daed9f6af6a10be3ccb60a6b4718880e0
Olivia Abrant <oliviaabrant@engr2-24-20-dhcp.int.colorado.edu>
2015-03-10 10:22:21 -0600

    completed user list and listItem

--- contents/js/user/listItem.js
+++ contents/js/user/listItem.js
@@ -1,11 +1,23 @@
 var UserListItem = React.createClass({
 
+    handleClick: function(event) {
+        console.log('an item is clicked', this.props.user)
+        this.props.onListItemClicked(this.props.user.user_id)
+    },
+
     render: function() {
 
-        return (
-            <div>
-                TODO
+        var user = this.props.user
+
+        return ( 
+            <div className="user row" >
+                <div className="doctorName six columns" onClick={this.handleClick}>
+                    {user.name}                    
+                </div>
+                <div className="userCity six columns">
+                    {user.city}   
+                </div>
             </div>
         )
-    }
+  }
 })
