76504a9d623199b5a98a51d5e80a1bd8b20cef2e
Domenic Murtari <domenic.murtari@gmail.com>
2015-03-10 10:03:41 -0600

    Restaurants working

--- contents/js/restaurant/listItem.js
+++ contents/js/restaurant/listItem.js
@@ -1,11 +1,23 @@
 var RestaurantListItem = React.createClass({
 
+    handleClick: function(event) {
+        console.log('an item is clicked', this.props.restaurant)
+        this.props.onListItemClicked(this.props.restaurant.business_id)
+    },
+
     render: function() {
 
-        return (
-            <div>
-                TODO
+        var restaurant = this.props.restaurant
+
+        return ( 
+            <div className="restaurant row" >
+                <div className="restaurantName six columns" onClick={this.handleClick}>
+                    {restaurant.name}                    
+                </div>
+                <div className="restaurantCity six columns">
+                    {restaurant.city}   
+                </div>
             </div>
         )
-    }
+  }
 })
