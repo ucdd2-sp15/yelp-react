ed1776b0acddffaa3e3879009618dbf6258df67f
BrianNewsom <brian.newsom@colorado.edu>
2015-03-11 20:31:25 -0600

    update doctor view

--- contents/js/doctor/view.js
+++ contents/js/doctor/view.js
@@ -5,15 +5,27 @@ var DoctorView = React.createClass({
     },
 
     render: function() {
-        
-        if (this.state.doctor){
 
-            return ( 
+        if (this.state.doctor){
+            var cats = this.state.doctor.categories
+            var categories = ''
+            for( var i = 0 ; i < cats.length ; i++){
+                categories += ' ' + cats[i]
+            }
+
+            return (
                 <div className="doctorView" >
                     <h2 className="doctorName">
                         {this.state.doctor.name}
                     </h2>
                     {this.state.doctor.full_address}
+                    <p>State: {this.state.doctor.state} </p>
+                    <p><b>Stars: {this.state.doctor.stars} </b> </p>
+                    <p>Review Count: {this.state.doctor.review_count} </p>
+                    <p>Categories: {categories} </p>
+
+
+
                 </div>
             )
 
