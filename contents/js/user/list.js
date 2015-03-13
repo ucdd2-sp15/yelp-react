a284820e1c31e36be93d5ed266f568e489490497
Michael <mike2457@gmail.com>
2015-03-13 10:26:42 -0600

    Adding listUser

--- contents/js/user/list.js
+++ contents/js/user/list.js
@@ -1,11 +1,50 @@
-var UserList = React.createClass({
+var UserList = React.createClass({displayName: 'UserList',
+
+    getInitialState: function() {
+        return {data: []};
+    },
+
+    componentDidMount: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            success: function(data) {
+                this.setState({
+                    data: data
+                });
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+
+    handleListItemClicked: function(user_id) {
+        var user = _.find(this.state.data, {user_id: user_id})
+        this.refs.userView.setState({user: user})
+    },
+
 
     render: function() {
 
+        var self = this
+
+        var users = this.state.data.map(function(user) {
+            return (
+                <UserListItem user={user} onListItemClicked={self.handleListItemClicked}/>
+                )
+        })
+
         return (
-            <div>
-                TODO
+            <div className="userPage">
+                <div className="userList six columns">
+                    {users}
+                </div>
+                <div className="userView six columns">
+                    <UserView ref="userView"/>
+                </div>
             </div>
         )
     }
 })
+
---
+++ contents/js/user/list.js~
@@ -0,0 +1,11 @@
+var UserList = React.createClass({
+
+    render: function() {
+
+        return (
+            <div>
+                TODO
+            </div>
+        )
+    }
+})
