4c05532d51be72411e639f3fd0b8ad62cb31d5d0
Your Name <Tahani@Tahani-Almanies-MacBook-Pro.local>
2015-03-12 19:22:36 -0600

    completed view pages in yelp-react

--- contents/js/restaurant/view.js
+++ contents/js/restaurant/view.js
@@ -10,10 +10,13 @@ var RestaurantView = React.createClass({
 
             return (
                 <div className="restaurantView" >
-                    <h2 className="restaurantName">
+                    <h3 className="restaurantName">
                         {this.state.restaurant.name}
-                    </h2>
-                    {this.state.restaurant.full_address}
+                    </h3>
+                    <b> Rating : </b> {this.state.restaurant.stars}  stars<br/>   
+                    <b> Address : </b> {this.state.restaurant.full_address} <br/>
+                    <b> Type : </b> {this.state.restaurant.type} <br/>   
+                    <b> Review count : </b> {this.state.restaurant.review_count} <br/>
                 </div>
             )
 
