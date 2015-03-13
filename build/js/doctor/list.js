6ef69b90dfe96903094372667229407a04244751
Domenic Murtari <domenic.murtari@gmail.com>
2015-03-13 17:31:45 -0600

    Built to deploy

--- build/js/doctor/list.js
+++ build/js/doctor/list.js
@@ -3,7 +3,7 @@ var DoctorList = React.createClass({displayName: 'DoctorList',
     getInitialState: function() {
         return {data: []};
     },
-  
+
     componentDidMount: function() {
         $.ajax({
             url: this.props.url,
@@ -29,15 +29,21 @@ var DoctorList = React.createClass({displayName: 'DoctorList',
         var self = this
 
         var doctors = this.state.data.map(function (doctor) {
-        
-          return (    
+
+          return (
             <DoctorListItem doctor={doctor} onListItemClicked={self.handleListItemClicked}/>
             )
-        })        
+        })
 
         return (
             <div className="doctorPage">
                 <div className="doctorList six columns">
+                    <div className="six columns">
+                        <h4> Name </h4>
+                    </div>
+                    <div className="six columns">
+                        <h4> Location </h4>
+                    </div>
                     {doctors}
                 </div>
                 <div className="doctorView six columns">
--- build/js/restaurant/view.js
+++ build/js/restaurant/view.js
@@ -1,11 +1,36 @@
 var RestaurantView = React.createClass({
 
+    getInitialState: function() {
+        return {restaurant: null}
+    },
+
     render: function() {
 
-        return (
-            <div>
-                TODO
-            </div>
-        )
-    }
+        if (this.state.restaurant){
+            var openStr = 'Closed Now :('
+            if (this.state.restaurant.open){
+                openStr= 'Open Now!'
+            }
+
+            return (
+                <div className="restaurantView" >
+                    <h2 className="restaurantName">
+                        {this.state.restaurant.name}
+                    </h2>
+                    <p> {this.state.restaurant.full_address} </p>
+                    <p> <b> {openStr} </b> </p>
+                    <p> Stars: {this.state.restaurant.stars} </p>
+                    <p> Attire: {this.state.restaurant.attributes.Attire} </p>
+
+
+                </div>
+            )
+
+        }else{
+
+            return (
+                <h2>Click an item to see details</h2>
+            )
+        }
+  }
 })
